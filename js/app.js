6da06cb718dd0e3b8e3276f21e7471be100345a2
Raul Delgado <rdelgado1101@gmail.com>
2013-10-09 11:46:12 -0500

    prueba tiger
    prueba template tiger

--- js/app.js
+++
@@ -1,7 +0,0 @@
-﻿var myScroll;
-function loaded() {
-    myScroll = new iScroll('wrapper');
-}
-
-document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false); 
-document.addEventListener('DOMContentLoaded', function () { setTimeout(loaded, 200); }, false); 
